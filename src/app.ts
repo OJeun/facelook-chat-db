292aef35e39a8fcbd46c47c9d494b7cfae6c614c
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-11-12 19:54:36 -0800

    Create the function to start the server

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as DB from './db';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRouter from './routes/auth';
+import chatRouter from './routes/chat';
+import friendRouter from './routes/friend';
+import groupRouter from './routes/group';
+import invitationRouter from './routes/invitation';
+import { run } from 'node:test';
+
+
+
+
+dotenv.config();
+
+const runServer = async () => {
+    const app = express();
+    const sequelize = DB.init();
+    
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.json());
+    app.use(cors());
+
+    app.use('/api/auth', authRouter);
+    app.use('/api/chat', chatRouter);
+    app.use('/api/friend', friendRouter);
+    app.use('/api/group', groupRouter);
+    app.use('/api/invitation', invitationRouter);
+
+    app.listen(app.get('port'), () => {
+        console.log(`Server is running on port ${app.get('port')}`);
+    }
+    );
+};
+
+runServer();
+
