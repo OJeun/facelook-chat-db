403ab46583faaee23a8d47e527a1c9ef280126d2
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-11-12 18:10:05 -0800

    Add relationship between Group and Chat

--- src/db/associations.ts
+++ src/db/associations.ts
@@ -8,20 +8,11 @@ import { Invitation } from "./models/invitation";
 export default function setupAssociations(): void {
   // Find all friends for a user (where userId in Friend matches the user's ID)
   User.hasMany(Friend, { foreignKey: "userId", as: "friends" });
-
-  // Find all users who have this user as a friend (where friendId in Friend matches the user's ID)
   User.hasMany(Friend, { foreignKey: "friendId" });
-
-  // Find all groups a user belongs to
   User.hasMany(UserGroup, { foreignKey: "userId", as: "groups" });
-
-  // Find all chats associated with a specific user
   User.hasMany(Chat, { foreignKey: "userId", as: "chats" });
 
-  // Find all users in a specific group
   Group.hasMany(UserGroup, { foreignKey: "groupId", as: "users" });
-
-  // Find all chats associated with a specific group
   Group.hasMany(Chat, { foreignKey: "groupId", as: "chats" });
 
   // Invitation relationships
@@ -34,6 +25,7 @@ export default function setupAssociations(): void {
   User.hasMany(Invitation, { foreignKey: "senderId", as: "sentInvitations" });
   // A group can have many invitations
   Group.hasMany(Invitation, { foreignKey: "groupId", as: "groupInvitations" });
+  Chat.hasOne(Group, { foreignKey: "lastChatId", as: 'groupWithLastMessage', });
 
   // Join table relationships
   Friend.belongsTo(User, { foreignKey: "userId" });
